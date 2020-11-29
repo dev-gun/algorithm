a776818fbbab8e5319a6d99e6de27735fd204ab3
gunkim <gunkim0318@gmail.com>
2020-11-29 20:41:00 +0900

    선형 검색, 보초법을 통한 성능 개선

--- src/algorithm/search/LinearSearch.java
+++ src/algorithm/search/LinearSearch.java
@@ -2,19 +2,20 @@ package algorithm.search;
 
 public class LinearSearch {
     public static void main(String[] args) {
-        int[] arr = {1, 6, 8, 3, 7, 9, 2, 4, 5};
+        //보초법을 위한 맨 뒤 더미 값 추가.
+        int[] arr = {1, 6, 8, 3, 7, 9, 2, 4, 5, -1};
 
         int keyword = 7;
 
+        arr[arr.length-1] = keyword;
         int keywordIndex = getIndex(arr, keyword);
         System.out.printf("%d의 검색 결과 위치는 %d입니다.%n", keyword, keywordIndex);
     }
     public static int getIndex(int[] arr, int keyword){
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] == keyword){
-                return i;
-            }
+        int i = 0;
+        while (arr[i] != keyword) {
+            i++;
         }
-        return -1;
+        return i == arr.length-1?-1:i;
     }
 }
