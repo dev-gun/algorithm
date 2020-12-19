a04edba99d4fd29dc45b6d488ff89e6c77bb1789
gunkim0318 <gunkim0318@gmail.com>
2020-12-19 23:29:41 +0900

    병합 정렬 초안

--- src/algorithm/sort/MergeSort.java
+++ src/algorithm/sort/MergeSort.java
@@ -2,6 +2,27 @@ package algorithm.sort;
 
 public class MergeSort {
     public static void main(String[] args){
+        int[] a = {2, 4, 6, 8, 11, 13};
+        int[] b = {1, 2, 3, 4, 9, 16, 21};
 
+        int[] c = new int[a.length+b.length];
+
+        int pa = 0;
+        int pb = 0;
+        int pc = 0;
+
+        while(pa < a.length && pb < b.length){
+            c[pc++] = (a[pa] > b[pb]) ? b[pb++]:a[pa++];
+        }
+        while(pa < a.length){
+            c[pc++] = a[pa++];
+        }
+        while(pb < b.length){
+            c[pc++] = b[pb++];
+        }
+
+        for(int val: c){
+            System.out.print(val+", ");
+        }
     }
 }
