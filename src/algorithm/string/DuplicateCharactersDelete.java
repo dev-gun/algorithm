ff5b9db83b64c0dd79b4914df86bd202ef608e5c
gunkims <gunkim.dev@gmail.com>
2021-10-08 10:44:38 +0900

    [문자열] 중복 문자 제거 완료

---
+++ src/algorithm/string/DuplicateCharactersDelete.java
@@ -0,0 +1,18 @@
+package algorithm.string;
+
+import java.util.Scanner;
+
+public class DuplicateCharactersDelete {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            if(str.indexOf(str.charAt(i)) == i) {
+                result += str.charAt(i);
+            }
+        }
+        System.out.println(result);
+    }
+}
