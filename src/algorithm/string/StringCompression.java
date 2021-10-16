56a301871af4186fdd0eb6bcae9e583a4f6eeac5
gunkims <gunkim.dev@gmail.com>
2021-10-17 01:00:49 +0900

    문제 출처 추가

--- src/algorithm/string/StringCompression.java
+++ src/algorithm/string/StringCompression.java
@@ -2,6 +2,8 @@ package algorithm.string;
 
 import java.util.Scanner;
 
+//인프런-자바(Java) 알고리즘 문제풀이 : 코딩테스트 대비
+//문제 : 문자열 압축
 public class StringCompression {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
