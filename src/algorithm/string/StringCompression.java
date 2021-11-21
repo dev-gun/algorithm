248ab07a01307105e1f34ad68af0cb72a340820b
gunkim <gunkim.dev@gmail.com>
2021-11-21 23:51:01 +0900

    [문자열] 문자열 압축 마지막 문자는 처리되지 않는 문제 수정

--- src/algorithm/string/StringCompression.java
+++ src/algorithm/string/StringCompression.java
@@ -10,16 +10,15 @@ public class StringCompression {
         String str = in.next();
 
         String answer = "";
+        str += " ";
         int cnt = 1;
         for(int i = 0; i < str.length() - 1; i++) {
             if(str.charAt(i) == str.charAt(i+1)) {
                 cnt++;
             } else {
                 answer += str.charAt(i);
-                if(cnt > 1) {
-                    answer += String.valueOf(cnt);
-                    cnt = 1;
-                }
+                if(cnt > 1) answer += String.valueOf(cnt);
+                cnt = 1;
             }
         }
         System.out.println(answer);
