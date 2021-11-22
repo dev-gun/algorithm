12c08b83fd08d65cf0fa4438088e71abf1dc06c3
gunkims <gunkim.dev@gmail.com>
2021-11-22 09:46:10 +0900

    [배열] 피보나치 수열 완료

---
+++ src/algorithm/array/Fibonacci.java
@@ -0,0 +1,21 @@
+package algorithm.array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        arr[0] = 1;
+        arr[1] = 1;
+
+        for (int i = 2; i < n; i++) {
+            int prev = arr[i - 1];
+            int preprev = arr[i - 2];
+            arr[i] = (prev + preprev);
+        }
+        System.out.println(Arrays.toString(arr).replaceAll("]", "").replaceAll("\\[", "").replaceAll(",", ""));
+    }
+}
