ec19ee78b0cf5240f8afece93bc4aae032fbbc67
gunny <45007556+gunkim@users.noreply.github.com>
2022-12-30 21:10:14 +0900

    Time: 277 ms (79.45%), Space: 36.6 MB (99.39%) - LeetHub

--- 0001-two-sum/0001-two-sum.kt
+++ 0001-two-sum/0001-two-sum.kt
@@ -3,9 +3,8 @@ class Solution {
         for ((i, left) in nums.withIndex()) {
             for ((j, right) in nums.withIndex()) {
                 if(i == j) break
-                
-                val num = left + right
-                if(num == target) {
+
+                if((left + right) == target) {
                     return intArrayOf(i, j)
                 }
             }
