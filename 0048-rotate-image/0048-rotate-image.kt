2c3a37a19ddc386e9bb13bcf5eecf89f27cb730a
Gun Kim <45007556+gunkim@users.noreply.github.com>
2023-10-24 23:23:44 +0900

    Time: 169 ms (42.79%), Space: 35.7 MB (29.35%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun rotate(matrix: Array<IntArray>): Unit {
+        val n = matrix.size
+        for (i in 0 until n / 2) {
+            for (j in 0 until (n + 1) / 2) {
+                val temp = matrix[i][j]
+                matrix[i][j] = matrix[n - j - 1][i]
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1]
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1]
+                matrix[j][n - i - 1] = temp
+            }
+        }
+    }
+}
